7aa3e795a49c9b7b6976b4261fba1ddb1eb0d35e
jargyres <jta4208@gmail.com>
2019-11-25 12:36:38 -0700

    updated adi to receive at multiple ports

--- implementADI.py
+++ implementADI.py
@@ -1,15 +1,23 @@
 from ADI import ADI
 import time
-
+import numpy as np
 
 sdr = ADI()
 
-sdr.set_rf_frequency(2400000000)
+sdr.set_rf_frequency(3095000000)
+
 sdr.set_samplerate(30e6)
-sdr.set_tx_frequency(2400000000)
-sdr.set_gain_tx(-40)
+
+sdr.set_tx_frequency(3095000000)
+
+sdr.set_gain_tx(-50)
+
 sdr.createSignal()
 
-data = sdr.transmit_and_receive(0, 0)
+sdr.tx_transmit(0, 1)
+
+time.sleep(0.2)
+
+data = sdr.receive([0, 1, 2, 3])
 
 sdr.plot_revieved(data)
